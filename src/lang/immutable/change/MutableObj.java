73ffd2b567eb8fd680aa3d0df1d50508191ea122
minji <heunhanireum@gmail.com>
2025-06-18 23:34:34 +0900

    250618

--- src/lang/immutable/change/MutableObj.java
+++ src/lang/immutable/change/MutableObj.java
@@ -2,5 +2,21 @@ package lang.immutable.change;
 
 public class MutableObj {
 
-    private int value
+    private int value;
+
+    public MutableObj(int value) {
+        this.value = value;
+    }
+
+    public void add(int addValue) {
+        value = value + addValue;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
