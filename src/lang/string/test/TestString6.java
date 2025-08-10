c29508113c0c60ddef0bb01802d1b5af97f642c7
minji <heunhanireum@gmail.com>
2025-08-11 00:31:45 +0900

    20250810

---
+++ src/lang/string/test/TestString6.java
@@ -0,0 +1,31 @@
+package lang.string.test;
+
+public class TestString6 {
+    public static void main(String[] args) {
+        String str = "start hello java, hello spring, hello jpa";
+        String key = "hello";
+
+        /*
+        int count = 0;
+
+        while (true) {
+            int index = str.indexOf(key);
+            if (index != -1) {
+                count += 1;
+                str = str.substring(index + key.length());
+            } else {
+                break;
+            }
+        }
+        System.out.println("count = " + count);
+        */
+
+        int count = 0;
+        int index = str.indexOf(key);
+        while (index >= 0) {
+            index = str.indexOf(key, index + 1);
+            count++;
+        }
+        System.out.println("count = " + count);
+    }
+}
