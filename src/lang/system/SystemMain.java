260642f02d24502b9f74382af80a9224f108033a
minji <heunhanireum@gmail.com>
2025-08-19 00:07:59 +0900

    20250818

---
+++ src/lang/system/SystemMain.java
@@ -0,0 +1,35 @@
+package lang.system;
+
+import java.util.Arrays;
+
+public class SystemMain {
+    public static void main(String[] args) {
+        // 현재 시간(밀리초)를 가져온다.
+        long currentTimeMillis = System.currentTimeMillis();
+        System.out.println("currentTimeMillis = " + currentTimeMillis);
+
+        // 현재 시간(나노초)를 가져온다.
+        long currentTimeNano = System.nanoTime();
+        System.out.println("currentTimeNano = " + currentTimeNano);
+
+        // 환경 변수를 읽는다.
+        System.out.println("getenv = " + System.getenv());
+
+        // 시스템 속성을 읽는다.
+        System.out.println("properties = " + System.getProperties());
+        System.out.println("Java version: " + System.getProperty("java.version"));
+
+        // 배열을 고속으로 복사한다. (시스템 레벨에 던져서 복사 -> 운영체제나 하드웨어 레벨에서 통째로 복사, loop문을 돌리지 않기 때문에 빠름)
+        char[] originalArray = {'h', 'e', 'l', 'l', 'o'};
+        char[] copiedArray = new char[5];
+        System.arraycopy(originalArray, 0, copiedArray, 0, originalArray.length);
+
+        // 배열 출력
+        System.out.println("copiedArray = " + copiedArray);
+        System.out.println("Arrays.toString = " + Arrays.toString(copiedArray));
+
+        // 프로그램 종료
+        System.exit(0); // 가급적 사용하지 말자(프로그램이 의도치 않게 종료됨)
+        System.out.println("hello");
+    }
+}
